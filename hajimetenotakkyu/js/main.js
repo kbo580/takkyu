996141fad0406325e35eb5c2b7efa4d32ff15dd4
kbo580 <61081989+kbo580@users.noreply.github.com>
2023-10-03 20:39:03 +0900

    Add files via upload

---
+++ hajimetenotakkyu/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('#open').on('click', function(){
+    $('.sp-nav-menu').addClass('active');
+  });
+
+  $('#close').on('click', function(){
+    $('.sp-nav-menu').removeClass('active');
+  });
+
+
+
+
+
+
+
+});
